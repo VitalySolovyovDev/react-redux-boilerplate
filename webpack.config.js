c57cc260982f243d47b0944e23a506a65195340b
Vitlay Solovyov <vitaly.solovyov@tibbo.com>
2020-03-04 22:22:38 +0300

    Add package.json, webpack.config.js, .babelrc, index.html, index.js

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const webpack = require("webpack");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    watchOptions: {
+        aggregateTimeout: 300,
+        poll: 1000
+    },
+    devtool: "source-map",
+    entry: {
+        "bundle": "./src/index.js"
+    },
+    plugins: [
+        new CleanWebpackPlugin({
+            cleanAfterEveryBuildPatterns: ["dist"]
+        }),
+        new HtmlWebpackPlugin({
+            title: "App build development",
+            filename: `${__dirname}/dist/index.html`,
+            template: `${__dirname}/src/index.html`
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: ["babel-loader"],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/i,
+                use: [
+                    {
+                        loader: "url-loader",
+                        options: {
+                            limit: 8192
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(ttf|eot|woff|woff2)$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: `${__dirname}/src/fonts/[name].[ext]`
+                    }
+                }
+            },
+            {
+                test: /\.(css)$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    output: {
+        filename: "[name].js",
+        path: `${__dirname}/dist/`
+    }
+};
